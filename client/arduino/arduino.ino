415a86f1c38c38742b98d9e4e4f504acf9c0cf43
kostik <97781288+konstict@users.noreply.github.com>
2026-01-23 15:42:22 +0300

    Update arduino.ino

--- client/arduino/arduino.ino
+++ client/arduino/arduino.ino
@@ -1,4 +1,4 @@
-void setup() {
+void setup() { // при запуске, инициализация порта и Serial (для print)
   pinMode(A0, INPUT);
   Serial.begin(9600);
 }
@@ -15,23 +15,23 @@ int sumValue2 = 0;
 int sumSum2 = 0;
 int sumCounter2 = 0;
 
-void loop() {
-  if (millis()- lastTime >= 5){
-    int value = map(analogRead(A0), 0, 1024, 0, 255);
+void loop() { // главный цикл в ардуино (вызывается каждый тик)
+  if (millis() - lastTime >= 5){ // установка точного кд в 5 миллисекунд
+    int value = map(analogRead(A0), 0, 1024, 0, 255); // получаем значение и переводим границы в 0-255
 
-    sumSum = sumSum + value;
-    sumCounter++;
-    if (sumCounter >= 5){
+    sumSum = sumSum + value; // для усреднения значений - суммируем их
+    sumCounter++; // считаем количество суммирования
+    if (sumCounter >= 5){ // находим среднее арифметическое значение и обнуляем переменные
       sumValue = sumSum / sumCounter;
       sumSum = 0;
       sumCounter = 0;
     }
 
-    int diff = sumValue - sumValuePrev;
-    sumValuePrev = sumValue;
-    int newDiff = diff * diff;
+    int diff = sumValue - sumValuePrev; // операция дифференцирования
+    sumValuePrev = sumValue; // находим разность между текущим значением и предыдущим
+    int newDiff = diff * diff; // берём в квадрат
 
-    sumSum2 = sumSum2 + newDiff;
+    sumSum2 = sumSum2 + newDiff; // повторяем взятие усреднённых значений
     sumCounter2++;
     if (sumCounter2 >= 5){
       sumValue2 = sumSum2 / sumCounter2;
@@ -39,14 +39,14 @@ void loop() {
       sumCounter2 = 0;
     }
 
-    if (sumValue2 >= 500){
-      if (millis() - lastTimePulse >= 400){
-        Serial.println(60000 / (millis() - lastTimePulse));
-        lastTimePulse = millis();
+    if (sumValue2 >= 500){ // 500 - константа, значение выше или равно константе - можем регнуть удар
+      if (millis() - lastTimePulse >= 400){ // если с предыдущего удара сердца прошло более или равно 0.4 секунд, то
+        Serial.println(60000 / (millis() - lastTimePulse)); // находим и регаем пульс (количество ударов в минуту)
+        lastTimePulse = millis(); // записываем время последнего удара
       }
     }
     // Serial.println(value);
 
-    lastTime = millis();
+    lastTime = millis(); // итерация цикла завершена, записываем время его конца
   }
 }
